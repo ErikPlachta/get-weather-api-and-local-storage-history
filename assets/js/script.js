39fb1cca15fffcb89d035f0f60de75471c168d7a
Erik Plachta <eplachta@gmail.com>
2021-12-07 18:22:06 -0500

    add test function to verify api connection

--- assets/js/script.js
+++ assets/js/script.js
@@ -6,5 +6,44 @@
 
 
 // https://home.openweathermap.org/api_keys
-const api ="d5291050dfed6abda18c09f0e663326d";
+const apiKey ="d5291050dfed6abda18c09f0e663326d";
 
+
+// api.openweathermap.org/data/2.5/forecast?q={city name}&appid={API key}
+
+/*----------------------------------------------------------------------------*/
+//-- GETTING DATA
+
+function _get_City(cityName) {
+    let forecast = fetch('http://api.openweathermap.org/data/2.5/forecast?q='+cityName+'&appid='+apiKey)
+                    .then(function(response) {
+                     // request was successful
+                        if (response.ok) {
+                            response.json().then(function(data) {
+                            console.log(data);
+                            });
+                        } 
+                        // general error
+                        else {
+                            consoel.log('Error: Data Not Found');
+                        }
+                    })
+                    // try catch error
+                    .catch(function(error) {
+                        console.log("Unable to connect to URL")
+                    });
+    return forecast;
+};
+
+
+
+/*----------------------------------------------------------------------------*/
+//-- DATABASE MANAGEMENT
+
+
+
+/*----------------------------------------------------------------------------*/
+//-- TESTING
+
+let cityName = 'Charlotte';
+console.log(_get_City(cityName));
